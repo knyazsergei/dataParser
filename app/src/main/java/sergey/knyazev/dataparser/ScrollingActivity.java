0379f434ac4beec8e47ebad5a0a27fa2e3c11975
Knyazev Sergey <yaknyazsergei@yandex.ru>
2017-06-14 20:53:55 +0300

    Добавлена загрузка xml документа

--- app/src/main/java/sergey/knyazev/dataparser/ScrollingActivity.java
+++ app/src/main/java/sergey/knyazev/dataparser/ScrollingActivity.java
@@ -1,16 +1,29 @@
 package sergey.knyazev.dataparser;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    public TextView loadedText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,28 +37,80 @@ public class ScrollingActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                loadedText = (TextView) findViewById(R.id.textContent);
+                loadedText.setText("Загрузка...");
+                new ProgressTask().execute("https://lifehacker.ru/feed/");
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_scrolling, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ProgressTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... path) {
+
+            String content;
+            try{
+                content = getContent(path[0]);
+            }
+            catch (IOException ex){
+                content = ex.getMessage();
+            }
+
+            return content;
+        }
+        @Override
+        protected void onPostExecute(String content) {
+            loadedText.setText("Данные загружены");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            loadedText.setText(content);
+            URL url = new URL("http://image10.bizrate-images.com/resize?sq=60&uid=2216744464");
+            Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            imageView.setImageBitmap(bmp);
+
+        }
+
+        private String getContent(String path) throws IOException {
+            BufferedReader reader=null;
+            try {
+                URL url = new URL(path);
+                HttpsURLConnection c=(HttpsURLConnection)url.openConnection();
+                c.setRequestMethod("GET");
+                c.setReadTimeout(10000);
+                c.connect();
+                reader= new BufferedReader(new InputStreamReader(c.getInputStream()));
+                StringBuilder buf=new StringBuilder();
+                String line = null;
+                while ((line=reader.readLine()) != null) {
+                    buf.append(line + "\n");
+                }
+                return(buf.toString());
+            }
+            finally {
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+        }
     }
 }
